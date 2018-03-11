db57b9e905b75156d705dde77e4497b1ce6d4500
dandresky <dandresky@gmail.com>
2018-03-10 20:15:21 -0700

    modified dropout values for v1 model

--- cats-dogs/src/cnn_model.py
+++ cats-dogs/src/cnn_model.py
@@ -12,22 +12,25 @@ def build_model(version, optimizer, input_shape):
 
 
 def build_model_v1(optimizer, input_shape):
-
+    '''
+    Model v1 is a shallow and narrow model compared to the well known VGG16
+    introduced with Imagenet challenges.
+    '''
     model = Sequential()
     model.add(Conv2D(32, (3, 3), input_shape=input_shape))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
+    model.add(Dropout(0.5))
 
     model.add(Conv2D(64, (3, 3)))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
+    model.add(Dropout(0.5))
 
     model.add(Conv2D(128, (3, 3)))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
+    model.add(Dropout(0.5))
 
     model.add(Flatten())
     model.add(Dense(256))
